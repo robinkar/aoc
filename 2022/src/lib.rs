6f96c004dfc7ad4c70fb980244f8fd1c0df83c6e
Robin Karlsson <61623634+robinkar@users.noreply.github.com>
2022-12-09 12:54:18 +0100

    Improve benchmark macro

--- 2022/src/lib.rs
+++ 2022/src/lib.rs
@@ -3,6 +3,10 @@
 /// # #[macro_use] extern crate aoc;
 /// let (result, elapsed) = bench!(1+2);
 /// println!("Expression took {} seconds to run", elapsed.as_secs());
+///
+/// let elapsed: Duration;
+/// let result = bench!(elapsed, 1+2);
+/// println!("Expression took {} seconds to run", elapsed.as_secs());
 /// ```
 #[macro_export]
 macro_rules! bench {
@@ -11,5 +15,11 @@ macro_rules! bench {
         let result = $x;
         let elapsed = start.elapsed();
         (result, elapsed)
+    }};
+    ($elapsed:ident, $x:expr) => {{
+        let start = std::time::Instant::now();
+        let result = $x;
+        $elapsed = start.elapsed();
+        result
     }};
 }
