de3695ad99d450a5ad10a4ec055a51e2026ee46d
Robin Karlsson <61623634+robinkar@users.noreply.github.com>
2022-12-09 12:07:15 +0100

    Add macro for benchmarking

---
+++ 2022/src/lib.rs
@@ -0,0 +1,15 @@
+/// Measures the `Duration` it take to run the given expression.
+/// ```
+/// # #[macro_use] extern crate aoc;
+/// let (result, elapsed) = bench!(1+2);
+/// println!("Expression took {} seconds to run", elapsed.as_secs());
+/// ```
+#[macro_export]
+macro_rules! bench {
+    ($x:expr) => {{
+        let start = std::time::Instant::now();
+        let result = $x;
+        let elapsed = start.elapsed();
+        (result, elapsed)
+    }};
+}
